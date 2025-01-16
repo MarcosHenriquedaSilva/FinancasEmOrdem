992dc77821683fd8aa705a1d42959f7c473b47e1
devmhs <mhs.02@hotmail.com>
2025-01-16 19:03:04 -0300

    Alterações na estrutura do projeto para melhorias

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
+import React from "react";
+
+
 const App = () => {
   return (
-    <div style={{ backgroundColor: "#f0f0f0", height: "100vh" }}>
-      <h1 style={{ textAlign: "center", paddingTop: "20px" }}>Projeto FinançasEmOrdem Funcional!</h1>
-    </div>
+    <div><h1>Hello Word</h1></div>
   );
 };
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";
-
-console.log("Iniciando a aplicação...");
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <App />
 );
-
-console.log("React foi inicializado com sucesso.");
