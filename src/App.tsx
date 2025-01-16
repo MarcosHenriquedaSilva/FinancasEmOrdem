34e47f694abdcc7ca3c1ea50805bd34bdfb69d66
devmhs <mhs.02@hotmail.com>
2025-01-16 16:37:11 -0300

    Normalização de terminações de linha

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div style={{ backgroundColor: "#f0f0f0", height: "100vh" }}>
+      <h1 style={{ textAlign: "center", paddingTop: "20px" }}>Projeto FinançasEmOrdem Funcional!</h1>
+    </div>
+  );
+};
+
+export default App;
