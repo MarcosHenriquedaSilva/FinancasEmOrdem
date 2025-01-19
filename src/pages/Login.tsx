67ea34f28eda7e0dfbbcd9b6bc7dffa1f06c798e
devmhs <mhs.02@hotmail.com>
2025-01-19 14:51:35 -0300

    Adiciona redirecionamento para Dashboard após login e logout

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,18 +1,56 @@
-import React from 'react';
-import './Login.css';
 
-const Login = () => {
+
+import './Login.css';
+import React, { useState } from "react";
+import { useAuth } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
+
+const Login: React.FC = () => {
+  const { login } = useAuth();
+  const navigate = useNavigate();
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    // Salva os dados no localStorage para persistência
+    localStorage.setItem("username", username);
+    localStorage.setItem("password", password);
+
+    login(); // Realiza o login
+    setUsername(""); // Limpa o campo de usuário
+    setPassword(""); // Limpa o campo de senha
+
+    // Redireciona para a página Dashboard após login
+    navigate("/dashboard");
+  };
+
   return (
-    <div className="login-container">
-      <div className="login-card">
-        <h1>FinançasEmOrdem</h1>
-        <form>
-          <input type="email" placeholder="E-mail" />
-          <input type="password" placeholder="Senha" />
-          <button type="submit">Entrar</button>
-          <a href="#">Esqueci minha senha</a>
-        </form>
-      </div>
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          <label htmlFor="username">Usuário</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Senha</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit">Entrar</button>
+      </form>
     </div>
   );
 };
