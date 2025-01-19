051b8bd0e927e009715dbc989a4f23841a14b6ec
devmhs <mhs.02@hotmail.com>
2025-01-19 14:51:02 -0300

    Implementa lógica de autenticação no contexto e gerenciamento de estado

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,36 @@
+// src/context/AuthContext.tsx
+import React, { createContext, useContext, useState } from "react";
+
+interface AuthContextType {
+  isAuthenticated: boolean;
+  login: () => void;
+  logout: () => void;
+}
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export const AuthProvider: React.FC = ({ children }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+
+  const login = () => {
+    setIsAuthenticated(true);
+  };
+
+  const logout = () => {
+    setIsAuthenticated(false); // Limpa o estado de autenticação
+  };
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = (): AuthContextType => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useAuth must be used within an AuthProvider");
+  }
+  return context;
+};
